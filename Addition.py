2546860e0c6840ac66adb472dbabd448348f47f4
balakrish2343 <balakrish2343@gmail.com>
2023-05-05 23:31:59 +0530

    Second commit

--- Addition.py
+++ Addition.py
@@ -1,5 +1,5 @@
-A = int(input('enter number 1:'))
-
-B = int(input('enter number 2:'))
-
-print("Addition:", A+B)
+s1 = 'Where are the snows of yesteryear?'
+s2 = s1.split()
+print(s2)
+s3=sorted(s2)
+print(s3)
